47a87fa5efea754450d4223d14a3877a18182952
thorbuckwel <thorbuckwell@gmail.com>
2017-11-14 11:28:06 -0500

    Initial commit

---
+++ Engine/CreatureData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    class CreatureData
+    {
+        // TODO Create the item class!!
+        List<item> Inventory = new List<Item>();
+    }
+}
